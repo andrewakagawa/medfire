505c82148e58826057a2d22aab01de9444fb7932
Andrew Akagawa <andrewakagawa@gmail.com>
2013-08-24 20:25:06 +0000

    updates to web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ app.use(express.logger());
 
 app.get('/', function(request, response) {
     buf=fs.readFileSync('index.html');
-    response.send(buf.toString('utf8', 0, buf.length);
+    response.send(buf.toString('utf8', 0, buf.length));
 });
 
 var port = process.env.PORT || 5000;
