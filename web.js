10af5153455340e0560ab4bb9e66febf0145e02e
Andrew Akagawa <andrewakagawa@gmail.com>
2013-08-24 19:38:02 +0000

    Updated web.js with html file

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    buf=fs.readFileSync('index.html');
+    response.send(buf.toString('utf8', 0, buf.length);
 });
 
 var port = process.env.PORT || 5000;
